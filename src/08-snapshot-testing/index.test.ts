8e45c17c66787704b4915116685f1dff59a38a85
Kate-Shepel <kateborichevskaya@gmail.com>
2024-10-20 12:53:00 +0200

    feat: add snapshot tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,33 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
-  test('should generate linked list from values 1', () => {
-    // Write your test here
+  test('should generate linked list from a mix of numbers, booleans, and strings', () => {
+    const testArr = [42, true, 'hello', false];
+    const expectedTestList = {
+      value: 42,
+      next: {
+        value: true,
+        next: {
+          value: 'hello',
+          next: {
+            value: false,
+            next: {
+              value: null,
+              next: null,
+            },
+          },
+        },
+      },
+    };
+    const generatedTestList = generateLinkedList(testArr);
+
+    expect(generatedTestList).toStrictEqual(expectedTestList);
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const testArr = [{ id: 1 }, null, { id: 2 }, { name: 'test' }];
+    const generatedTestList = generateLinkedList(testArr);
+
+    expect(generatedTestList).toMatchSnapshot();
   });
 });
